580dc00fc50db56c7eaa97b2761ccd71a57c9247
Quynh Nguyen <mr.quynh@gmail.com>
2019-05-17 14:25:06 +0700

    Restructure: add information of controller/service

--- ServiceComponent.cs
+++ ServiceComponent.cs
@@ -24,7 +24,7 @@ namespace net.vieapps.Services
 		/// <summary>
 		/// Starts the service
 		/// </summary>
-		/// <param name="args">The starting arguments</param>
+		/// <param name="args">The arguments</param>
 		/// <param name="initializeRepository">true to initialize the repository of the service</param>
 		/// <param name="nextAsync">The next action to run asynchronously</param>
 		void Start(string[] args = null, bool initializeRepository = true, Func<IService, System.Threading.Tasks.Task> nextAsync = null);
@@ -32,6 +32,7 @@ namespace net.vieapps.Services
 		/// <summary>
 		/// Stops the service
 		/// </summary>
-		void Stop();
+		/// <param name="args">The arguments</param>
+		void Stop(string[] args = null);
 	}
 }
