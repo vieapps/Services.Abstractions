66fbc4ad0bd4cca74f86ce12a547e9cb34dcad64
Quynh Nguyen <mr.quynh@gmail.com>
2019-05-17 14:25:14 +0700

    Restructure: add information of controller/service

---
+++ ControllerInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+namespace net.vieapps.Services
+{
+	/// <summary>
+	/// Presents information of a controller
+	/// </summary>
+	[Serializable]
+	public class ControllerInfo
+	{
+		public ControllerInfo() { }
+
+		/// <summary>
+		/// Gets or sets the identity
+		/// </summary>
+		public string ID { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of user on the host that running the controller
+		/// </summary>
+		public string User { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the host that running the controller
+		/// </summary>
+		public string Host { get; set; }
+
+		/// <summary>
+		/// Gets or sets the OS platform information of the host that running the controller
+		/// </summary>
+		public string Platform { get; set; }
+
+		/// <summary>
+		/// Gets or sets the running mode (Interactive or Background)
+		/// </summary>
+		public string Mode { get; set; }
+
+		/// <summary>
+		/// Gets or sets the avalable state
+		/// </summary>
+		public bool Available { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets the starting time
+		/// </summary>
+		public DateTime Timestamp { get; set; } = DateTime.Now;
+
+		/// <summary>
+		/// Gets or sets the extra information
+		/// </summary>
+		public Dictionary<string, string> Extra { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+	}
+}
---
+++ ServiceInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace net.vieapps.Services
+{
+	/// <summary>
+	/// Presents information of a service
+	/// </summary>
+	[Serializable]
+	public class ServiceInfo
+	{
+		public ServiceInfo() { }
+
+		/// <summary>
+		/// Gets or sets the name of the service
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the unique name of the service
+		/// </summary>
+		public string UniqueName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the identity of the controller that hosts and controls the service
+		/// </summary>
+		public string ControllerID { get; set; }
+
+		/// <summary>
+		/// Gets or sets the invoking information of the service
+		/// </summary>
+		public string InvokeInfo { get; set; }
+
+		/// <summary>
+		/// Gets or sets the starting time of the service
+		/// </summary>
+		public DateTime Timestamp { get; set; } = DateTime.Now;
+
+		/// <summary>
+		/// Gets or sets the available state of the service
+		/// </summary>
+		public bool Available { get; set; } = false;
+
+		/// <summary>
+		/// Gets or sets the running state of the service
+		/// </summary>
+		public bool Running { get; set; } = false;
+	}
+}
