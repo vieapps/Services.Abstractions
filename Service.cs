3327437305f08f5a07370cc33e7b65d1e8157fd8
Quynh Nguyen <mr.quynh@gmail.com>
2019-05-19 23:45:56 +0700

    Restructure: arguments of action-base authorization

--- Service.cs
+++ Service.cs
@@ -45,20 +45,7 @@ namespace net.vieapps.Services
 		/// <param name="cancellationToken">The cancellation token</param>
 		/// <returns></returns>
 		[WampProcedure("services.{0}.privileges.manage")]
-		Task<bool> CanManageAsync(IUser user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
-
-		/// <summary>
-		/// Gets the state that determines the user is able to manage or not
-		/// </summary>
-		/// <param name="requestInfo">The requesting information</param>
-		/// <param name="objectName">The name of the service's object</param>
-		/// <param name="systemID">The identity of the business system</param>
-		/// <param name="definitionID">The identity of the business entity definition</param>
-		/// <param name="objectID">The identity of the object</param>
-		/// <param name="cancellationToken">The cancellation token</param>
-		/// <returns></returns>
-		[WampProcedure("services.{0}.privileges.manages")]
-		Task<bool> CanManageAsync(RequestInfo requestInfo, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
+		Task<bool> CanManageAsync(User user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		/// Gets the state that determines the user is able to moderate or not
@@ -71,20 +58,7 @@ namespace net.vieapps.Services
 		/// <param name="cancellationToken">The cancellation token</param>
 		/// <returns></returns>
 		[WampProcedure("services.{0}.privileges.moderate")]
-		Task<bool> CanModerateAsync(IUser user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
-
-		/// <summary>
-		/// Gets the state that determines the user is able to moderate or not
-		/// </summary>
-		/// <param name="requestInfo">The requesting information</param>
-		/// <param name="objectName">The name of the service's object</param>
-		/// <param name="systemID">The identity of the business system</param>
-		/// <param name="definitionID">The identity of the business entity definition</param>
-		/// <param name="objectID">The identity of the object</param>
-		/// <param name="cancellationToken">The cancellation token</param>
-		/// <returns></returns>
-		[WampProcedure("services.{0}.privileges.moderates")]
-		Task<bool> CanModerateAsync(RequestInfo requestInfo, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
+		Task<bool> CanModerateAsync(User user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		/// Gets the state that determines the user is able to edit or not
@@ -97,20 +71,7 @@ namespace net.vieapps.Services
 		/// <param name="cancellationToken">The cancellation token</param>
 		/// <returns></returns>
 		[WampProcedure("services.{0}.privileges.edit")]
-		Task<bool> CanEditAsync(IUser user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
-
-		/// <summary>
-		/// Gets the state that determines the user is able to edit or not
-		/// </summary>
-		/// <param name="requestInfo">The requesting information</param>
-		/// <param name="objectName">The name of the service's object</param>
-		/// <param name="systemID">The identity of the business system</param>
-		/// <param name="definitionID">The identity of the business entity definition</param>
-		/// <param name="objectID">The identity of the object</param>
-		/// <param name="cancellationToken">The cancellation token</param>
-		/// <returns></returns>
-		[WampProcedure("services.{0}.privileges.edits")]
-		Task<bool> CanEditAsync(RequestInfo requestInfo, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
+		Task<bool> CanEditAsync(User user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		/// Gets the state that determines the user is able to contribute or not
@@ -123,20 +84,7 @@ namespace net.vieapps.Services
 		/// <param name="cancellationToken">The cancellation token</param>
 		/// <returns></returns>
 		[WampProcedure("services.{0}.privileges.contribute")]
-		Task<bool> CanContributeAsync(IUser user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
-
-		/// <summary>
-		/// Gets the state that determines the user is able to contribute or not
-		/// </summary>
-		/// <param name="requestInfo">The requesting information</param>
-		/// <param name="objectName">The name of the service's object</param>
-		/// <param name="systemID">The identity of the business system</param>
-		/// <param name="definitionID">The identity of the business entity definition</param>
-		/// <param name="objectID">The identity of the object</param>
-		/// <param name="cancellationToken">The cancellation token</param>
-		/// <returns></returns>
-		[WampProcedure("services.{0}.privileges.contributes")]
-		Task<bool> CanContributeAsync(RequestInfo requestInfo, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
+		Task<bool> CanContributeAsync(User user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		/// Gets the state that determines the user is able to view or not
@@ -149,20 +97,7 @@ namespace net.vieapps.Services
 		/// <param name="cancellationToken">The cancellation token</param>
 		/// <returns></returns>
 		[WampProcedure("services.{0}.privileges.view")]
-		Task<bool> CanViewAsync(IUser user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
-
-		/// <summary>
-		/// Gets the state that determines the user is able to view or not
-		/// </summary>
-		/// <param name="requestInfo">The requesting information</param>
-		/// <param name="objectName">The name of the service's object</param>
-		/// <param name="systemID">The identity of the business system</param>
-		/// <param name="definitionID">The identity of the business entity definition</param>
-		/// <param name="objectID">The identity of the object</param>
-		/// <param name="cancellationToken">The cancellation token</param>
-		/// <returns></returns>
-		[WampProcedure("services.{0}.privileges.views")]
-		Task<bool> CanViewAsync(RequestInfo requestInfo, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
+		Task<bool> CanViewAsync(User user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		/// Gets the state that determines the user is able to download the attachment files or not
@@ -175,19 +110,6 @@ namespace net.vieapps.Services
 		/// <param name="cancellationToken">The cancellation token</param>
 		/// <returns></returns>
 		[WampProcedure("services.{0}.privileges.download")]
-		Task<bool> CanDownloadAsync(IUser user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
-
-		/// <summary>
-		/// Gets the state that determines the user is able to download or not
-		/// </summary>
-		/// <param name="requestInfo">The requesting information</param>
-		/// <param name="objectName">The name of the service's object</param>
-		/// <param name="systemID">The identity of the business system</param>
-		/// <param name="definitionID">The identity of the business entity definition</param>
-		/// <param name="objectID">The identity of the object</param>
-		/// <param name="cancellationToken">The cancellation token</param>
-		/// <returns></returns>
-		[WampProcedure("services.{0}.privileges.downloads")]
-		Task<bool> CanDownloadAsync(RequestInfo requestInfo, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
+		Task<bool> CanDownloadAsync(User user, string objectName, string systemID, string definitionID, string objectID, CancellationToken cancellationToken = default(CancellationToken));
 	}
 }
