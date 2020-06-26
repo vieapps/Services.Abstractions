53aaa86062cfc3c6c900ee5558f66c4066ede592
Quynh Nguyen <mr.quynh@gmail.com>
2020-06-26 16:04:45 +0700

    (improv): add interface of syncable service

---
+++ Services/Service.Sync.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WampSharp.V2.Rpc;
+using Newtonsoft.Json.Linq;
+namespace net.vieapps.Services
+{
+	/// <summary>
+	/// Presents a syncable business service in the VIEApps NGX
+	/// </summary>
+	public interface ISyncableService : IService
+	{
+		/// <summary>
+		/// Processes the sync request
+		/// </summary>
+		/// <param name="requestInfo">The requesting information</param>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <returns>The JSON object that contains the result</returns>
+		[WampProcedure("services.{0}.sync")]
+		Task<JToken> SyncAsync(RequestInfo requestInfo, CancellationToken cancellationToken = default);
+	}
+}
