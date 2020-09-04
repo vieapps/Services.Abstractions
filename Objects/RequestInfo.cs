ba0de78a2d2963e84a4b94f8dcc646e0edcdaa16
Quynh Nguyen <mr.quynh@gmail.com>
2020-09-05 01:35:35 +0700

    (improv): add more helper properties

--- Objects/RequestInfo.cs
+++ Objects/RequestInfo.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Dynamic;
+using System.Globalization;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using net.vieapps.Components.Utility;
 namespace net.vieapps.Services
 {
@@ -79,7 +84,7 @@ namespace net.vieapps.Services
 		public Dictionary<string, string> Header { get; set; }
 
 		/// <summary>
-		/// Gets or sets the JSON body of the request (only available when verb is POST or PUT)
+		/// Gets or sets the body (JSON string) of the request (only available when verb is POST, PUT or PATCH)
 		/// </summary>
 		public string Body { get; set; }
 
@@ -92,6 +97,30 @@ namespace net.vieapps.Services
 		/// Gets or sets the identity of the correlation
 		/// </summary>
 		public string CorrelationID { get; set; }
+
+		/// <summary>
+		/// Gets the culture code (means language code)
+		/// </summary>
+		[JsonIgnore, XmlIgnore]
+		public string CultureCode => this.Query.TryGetValue("language", out var language) && !string.IsNullOrWhiteSpace(language) ? language : "vi-VN";
+
+		/// <summary>
+		/// Gets the culture info (for working with language, number, calendar, ...)
+		/// </summary>
+		[JsonIgnore, XmlIgnore]
+		public CultureInfo CultureInfo => CultureInfo.GetCultureInfo(this.CultureCode);
+
+		/// <summary>
+		/// Gets the body as JSON object
+		/// </summary>
+		[JsonIgnore, XmlIgnore]
+		public JToken BodyAsJson => this.Body?.ToJson() ?? new JObject();
+
+		/// <summary>
+		/// Gets the body as ExpandoObject object
+		/// </summary>
+		[JsonIgnore, XmlIgnore]
+		public ExpandoObject BodyAsExpandoObject => this.Body?.ToExpandoObject() ?? new ExpandoObject();
 		#endregion
 
 	}
